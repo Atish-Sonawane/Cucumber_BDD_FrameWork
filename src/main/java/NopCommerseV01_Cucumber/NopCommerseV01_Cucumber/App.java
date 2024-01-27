bf44fc3467efd344681bf1645b1049664ad9581f
Atish Dilip Sonawane <atishsonawane11121999@gmail.com>
2024-01-27 21:36:10 +0530

    first commit

---
+++ src/main/java/NopCommerseV01_Cucumber/NopCommerseV01_Cucumber/App.java
@@ -0,0 +1,13 @@
+package NopCommerseV01_Cucumber.NopCommerseV01_Cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/NopCommerseV01_Cucumber/NopCommerseV01_Cucumber/Demo.java
@@ -0,0 +1,10 @@
+package NopCommerseV01_Cucumber.NopCommerseV01_Cucumber;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("demo1");
+
+	}
+
+}
